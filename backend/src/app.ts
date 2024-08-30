9bf392049dd7bec1cd3dad2d6f593829a9e8d396
zoltan-antal <zoltan_antal@outlook.com>
2024-08-30 16:19:14 +0100

    [backend] Configure CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 require('express-async-errors');
 import router from './routes';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
 import configurePassport from './utils/passport';
+import { FRONTEND_URL } from './utils/config';
 
 const app = express();
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(cors());
+    break;
+
+  case 'production':
+    app.use(cors({ origin: FRONTEND_URL }));
+    break;
+}
 
 app.use(express.json());
 app.use(cookieParser());
--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
+export const FRONTEND_URL = process.env.FRONTEND_URL;
 export const NODE_ENV = process.env.NODE_ENV as 'development' | 'production';
