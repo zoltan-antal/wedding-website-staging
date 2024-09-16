feacfc653c56fedd16e139a39046fca5e0ab02cd
zoltan-antal <zoltan_antal@outlook.com>
2024-09-16 19:48:38 +0100

    [frontend] Set up login screen steps

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -1,15 +1,68 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import authService from '../../services/auth';
+import guestService from '../../services/guest';
 
-const Login = () => {
-  const navigate = useNavigate();
+interface EnterNameStepProps {
+  onNext: (firstName: string, lastName: string, hasPassword: boolean) => void;
+}
 
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [password, setPassword] = useState('');
+const EnterNameStep = ({ onNext }: EnterNameStepProps) => {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleNext = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const guestDetails = await guestService.findGuest(firstName, lastName);
+      setErrorMessage('');
+      console.log(guestDetails);
+      onNext(firstName, lastName, guestDetails.hasPassword);
+    } catch (error) {
+      setErrorMessage('Guest not found.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleNext}>
+      <label>
+        First name:
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <button type="submit">Next</button>
+      <p>{errorMessage}</p>
+    </form>
+  );
+};
+
+interface EnterPasswordStepProps {
+  firstName: string;
+  lastName: string;
+  onLogin: () => void;
+}
+
+const EnterPasswordStep = ({
+  firstName,
+  lastName,
+  onLogin,
+}: EnterPasswordStepProps) => {
+  const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await authService.login({
@@ -17,44 +70,146 @@ const Login = () => {
         lastName,
         password,
       });
+      setErrorMessage('');
       console.log(response);
+      onLogin();
     } catch (error) {
-      console.error(error);
+      setErrorMessage('Invalid password.');
     }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Next</button>
+      <p>{errorMessage}</p>
+    </form>
+  );
+};
+
+interface CreatePasswordStepProps {
+  firstName: string;
+  lastName: string;
+  onCreatePassword: (password: string) => void;
+}
+
+const CreatePasswordStep = ({
+  firstName,
+  lastName,
+  onCreatePassword,
+}: CreatePasswordStepProps) => {
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleCreatePassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match.');
+      return;
+    }
+    try {
+      const response = await guestService.createPassword(
+        firstName,
+        lastName,
+        password
+      );
+      setErrorMessage('');
+      console.log(response);
+      onCreatePassword(password);
+    } catch (error) {
+      setErrorMessage('Invalid password.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleCreatePassword}>
+      <h3>You're logging in for the first time. Please create a password.</h3>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Confirm password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Next</button>
+      <p>{errorMessage}</p>
+    </form>
+  );
+};
+
+const Login = () => {
+  const [step, setStep] = useState<
+    'enter-name' | 'enter-password' | 'create-password'
+  >('enter-name');
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+
+  const navigate = useNavigate();
+
+  const handleNext = (
+    firstName: string,
+    lastName: string,
+    hasPassword: boolean
+  ) => {
+    setFirstName(firstName);
+    setLastName(lastName);
+    if (hasPassword) {
+      setStep('enter-password');
+    } else {
+      setStep('create-password');
+    }
+  };
+
+  const handleLogin = () => {
+    navigate(-1);
+  };
+
+  const handleCreatePassword = async (password: string) => {
+    alert('Password created successfully!');
+    const response = await authService.login({
+      firstName,
+      lastName,
+      password,
+    });
+    console.log(response);
     navigate(-1);
   };
 
   return (
     <main>
       <h1>Guest login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          First name:
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label>
-          Last name:
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-
-        <button type="submit">Log in</button>
-      </form>
+      {step === 'enter-name' && <EnterNameStep onNext={handleNext} />}
+      {step === 'enter-password' && (
+        <EnterPasswordStep
+          firstName={firstName}
+          lastName={lastName}
+          onLogin={handleLogin}
+        />
+      )}
+      {step === 'create-password' && (
+        <CreatePasswordStep
+          firstName={firstName}
+          lastName={lastName}
+          onCreatePassword={handleCreatePassword}
+        />
+      )}
     </main>
   );
 };
--- frontend/src/services/guest.ts
+++ frontend/src/services/guest.ts
@@ -3,10 +3,30 @@ import config from '../utils/config';
 
 const baseUrl = `${config.BACKEND_URL}/guests`;
 
+async function findGuest(firstName: string, lastName: string) {
+  const response = await axios.get(baseUrl, {
+    params: { firstName, lastName },
+  });
+  return response.data;
+}
+
 async function me() {
   const requestConfig = { withCredentials: true };
   const response = await axios.get(`${baseUrl}/me`, requestConfig);
   return response.data;
 }
 
-export default { me };
+async function createPassword(
+  firstName: string,
+  lastName: string,
+  password: string
+) {
+  const response = await axios.post(`${baseUrl}/password`, {
+    firstName,
+    lastName,
+    password,
+  });
+  return response;
+}
+
+export default { findGuest, me, createPassword };
