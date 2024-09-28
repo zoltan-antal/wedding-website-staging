e9308ea6095c55418f8abb01ee509c00fd0d2f22
Zoltan Antal <zoltan_antal@outlook.com>
2024-09-28 14:00:43 +0100

    [backend] Disable all household GET endpoint

--- backend/src/routes/householdRoutes.ts
+++ backend/src/routes/householdRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import householdController from '../controllers/householdController';
+// import householdController from '../controllers/householdController';
 
 const householdRoutes = Router();
 
-householdRoutes.get('/', (req, res, next) => {
-  householdController.getAllHouseholds(req, res).catch(next);
-});
+// householdRoutes.get('/', (req, res, next) => {
+//   householdController.getAllHouseholds(req, res).catch(next);
+// });
+
+householdRoutes;
 
 export default householdRoutes;
