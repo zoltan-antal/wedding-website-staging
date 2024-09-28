fbb1d6dca8174f3727b4aa16719ad77c8deb2c34
Zoltan Antal <zoltan_antal@outlook.com>
2024-09-28 13:59:05 +0100

    [backend] Remove household creation endpoint

--- backend/src/services/householdService.ts
+++ backend/src/services/householdService.ts
@@ -1,18 +1,10 @@
 import { Household } from '../models';
-import {
-  HouseholdAttributes,
-  // HouseholdCreationAttributes,
-} from '../models/households';
+import { HouseholdAttributes } from '../models/households';
 
 const getAllHouseholds = async (): Promise<HouseholdAttributes[]> => {
   return await Household.findAll();
 };
 
-// const createHousehold = async ({ username }: HouseholdCreationAttributes) => {
-//   return await Household.create({ username });
-// };
-
 export default {
   getAllHouseholds,
-  // createHousehold
 };
