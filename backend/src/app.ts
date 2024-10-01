fdb810ca3ec75521abefd6ff47e70ce0fc82c629
zoltan-antal <zoltan_antal@outlook.com>
2024-10-01 22:44:07 +0100

    [backend] Set up email copy sending to internal email

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { Resend } from 'resend';
 require('express-async-errors');
 import router from './routes';
 import cookieParser from 'cookie-parser';
 import passport from 'passport';
 import configurePassport from './utils/passport';
-import { FRONTEND_URL, RESEND_API_KEY } from './utils/config';
+import { FRONTEND_URL } from './utils/config';
 
 const app = express();
 
@@ -36,22 +35,6 @@ app.use(passport.initialize());
 app.use('/', router);
 app.get('/ping', (_req, res) => {
   res.send('pong');
-});
-
-const resend = new Resend(RESEND_API_KEY);
-
-app.post('/email-test', (_req, res, next) => {
-  resend.emails
-    .send({
-      from: 'noreply@auto.ellazoltan.com',
-      to: 'info@ellazoltan.com',
-      subject: 'TEST',
-      html: '<p>This is a test email.</p>',
-    })
-    .then(() => {
-      res.send('Test email sent.');
-    })
-    .catch(next);
 });
 
 export default app;
--- backend/src/controllers/rsvpController.ts
+++ backend/src/controllers/rsvpController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import fs from 'fs';
 import path from 'path';
-import guestService from '../services/guestService';
+import { Resend } from 'resend';
 import { GuestAttributes } from '../models/guests';
+import guestService from '../services/guestService';
+import { RESEND_API_KEY } from '../utils/config';
 
 enum RsvpFormFieldNames {
   GuestsAttending = 'guestsAttending',
@@ -40,7 +42,7 @@ const submitRsvp = async (req: RsvpSubmissionRequest, res: Response) => {
   const guest = req.user as GuestAttributes;
   const dateTime = new Date();
 
-  const guestsAttending = await Promise.all(
+  const guestsAttendingFlattened = await Promise.all(
     Object.entries(formData.guestsAttending).map(
       async ([guestId, isComing]) => {
         const guest = await guestService.getGuest(Number(guestId));
@@ -48,10 +50,10 @@ const submitRsvp = async (req: RsvpSubmissionRequest, res: Response) => {
       }
     )
   );
-  const guestsAttendingJoined = guestsAttending.join(',');
+  const guestsAttendingFlattenedJoined = guestsAttendingFlattened.join(',');
   const csvLine = `"${guest.lastName},${
     guest.firstName
-  }",${dateTime.toISOString()},"${guestsAttendingJoined}",${
+  }",${dateTime.toISOString()},"${guestsAttendingFlattenedJoined}",${
     formData.requireAccommodation
   },${formData.accommodationPreference},${formData.willingToShareTent},${
     formData.requireTransport
@@ -71,6 +73,28 @@ const submitRsvp = async (req: RsvpSubmissionRequest, res: Response) => {
     );
   }
   fs.appendFileSync(csvFilePath, `${csvLine}\n`, 'utf8');
+
+  const resend = new Resend(RESEND_API_KEY);
+  const formDataBeautified = JSON.stringify(
+    {
+      submittedBy: `${guest.lastName},${guest.firstName}`,
+      submittedAt: dateTime.toLocaleString(),
+      ...formData,
+    },
+    null,
+    4
+  );
+  const emailBody = `<pre>${formDataBeautified}</pre>`;
+  try {
+    await resend.emails.send({
+      from: 'noreply@auto.ellazoltan.com',
+      to: 'info@ellazoltan.com',
+      subject: `RSVP form submitted by ${guest.lastName}, ${guest.firstName}`,
+      html: emailBody,
+    });
+  } catch (error) {
+    /* empty */
+  }
 
   return res.status(200).json({ message: 'Successfully saved RSVP form data' });
 };
