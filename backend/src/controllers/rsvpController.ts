a6026baf584dd31136613ec6e372978ee5ea031a
zoltan-antal <zoltan_antal@outlook.com>
2024-10-01 23:15:07 +0100

    [backend] Add error handling

--- backend/src/controllers/rsvpController.ts
+++ backend/src/controllers/rsvpController.ts
@@ -42,50 +42,59 @@ const submitRsvp = async (req: RsvpSubmissionRequest, res: Response) => {
   const guest = req.user as GuestAttributes;
   const dateTime = new Date();
 
-  const guestsAttendingFlattened = await Promise.all(
-    Object.entries(formData.guestsAttending).map(
-      async ([guestId, isComing]) => {
-        const guest = await guestService.getGuest(Number(guestId));
-        return `${guest?.firstName}:${isComing}`;
-      }
-    )
-  );
-  const guestsAttendingFlattenedJoined = guestsAttendingFlattened.join(',');
-  const csvLine = `"${guest.lastName},${
-    guest.firstName
-  }",${dateTime.toISOString()},"${guestsAttendingFlattenedJoined}",${
-    formData.requireAccommodation
-  },${formData.accommodationPreference},${formData.willingToShareTent},${
-    formData.requireTransport
-  },${formData.dietaryRequirements},${formData.interestedInMeetAndGreet},${
-    formData.interestedInPostWeddingWindDown
-  },${formData.comments}`;
+  let saveError: Error | null = null;
+  let emailError: Error | null = null;
 
-  const dataDirectoryPath = path.join(__dirname, '../..', 'data');
-  const csvFilePath = path.join(__dirname, '../..', 'data', 'rsvpData.csv');
-  if (!fs.existsSync(dataDirectoryPath)) {
-    fs.mkdirSync(dataDirectoryPath);
-  }
-  if (!fs.existsSync(csvFilePath)) {
-    fs.writeFileSync(
-      csvFilePath,
-      `submittedBy,submittedAt,${RsvpFormFieldNames.GuestsAttending},${RsvpFormFieldNames.RequireAccommodation},${RsvpFormFieldNames.AccommodationPreference},${RsvpFormFieldNames.WillingToShareTent},${RsvpFormFieldNames.RequireTransport},${RsvpFormFieldNames.DietaryRequirements},${RsvpFormFieldNames.InterestedInMeetAndGreet},${RsvpFormFieldNames.InterestedInPostWeddingWindDown},${RsvpFormFieldNames.Comments}\n`
-    );
-  }
-  fs.appendFileSync(csvFilePath, `${csvLine}\n`, 'utf8');
-
-  const resend = new Resend(RESEND_API_KEY);
-  const formDataBeautified = JSON.stringify(
-    {
-      submittedBy: `${guest.lastName},${guest.firstName}`,
-      submittedAt: dateTime.toLocaleString(),
-      ...formData,
-    },
-    null,
-    4
-  );
-  const emailBody = `<pre>${formDataBeautified}</pre>`;
   try {
+    const guestsAttendingFlattened = await Promise.all(
+      Object.entries(formData.guestsAttending).map(
+        async ([guestId, isComing]) => {
+          const guest = await guestService.getGuest(Number(guestId));
+          return `${guest?.firstName}:${isComing}`;
+        }
+      )
+    );
+    const guestsAttendingFlattenedJoined = guestsAttendingFlattened.join(',');
+    const csvLine = `"${guest.lastName},${
+      guest.firstName
+    }",${dateTime.toISOString()},"${guestsAttendingFlattenedJoined}",${
+      formData.requireAccommodation
+    },${formData.accommodationPreference},${formData.willingToShareTent},${
+      formData.requireTransport
+    },${formData.dietaryRequirements},${formData.interestedInMeetAndGreet},${
+      formData.interestedInPostWeddingWindDown
+    },${formData.comments}`;
+
+    const dataDirectoryPath = path.join(__dirname, '../..', 'data');
+    const csvFilePath = path.join(__dirname, '../..', 'data', 'rsvpData.csv');
+    if (!fs.existsSync(dataDirectoryPath)) {
+      fs.mkdirSync(dataDirectoryPath);
+    }
+    if (!fs.existsSync(csvFilePath)) {
+      fs.writeFileSync(
+        csvFilePath,
+        `submittedBy,submittedAt,${RsvpFormFieldNames.GuestsAttending},${RsvpFormFieldNames.RequireAccommodation},${RsvpFormFieldNames.AccommodationPreference},${RsvpFormFieldNames.WillingToShareTent},${RsvpFormFieldNames.RequireTransport},${RsvpFormFieldNames.DietaryRequirements},${RsvpFormFieldNames.InterestedInMeetAndGreet},${RsvpFormFieldNames.InterestedInPostWeddingWindDown},${RsvpFormFieldNames.Comments}\n`
+      );
+    }
+    fs.appendFileSync(csvFilePath, `${csvLine}\n`, 'utf8');
+  } catch (error) {
+    console.error(error);
+    saveError = error as Error;
+  }
+
+  try {
+    const formDataBeautified = JSON.stringify(
+      {
+        submittedBy: `${guest.lastName},${guest.firstName}`,
+        submittedAt: dateTime.toLocaleString(),
+        ...formData,
+      },
+      null,
+      4
+    );
+    const emailBody = `<pre>${formDataBeautified}</pre>`;
+
+    const resend = new Resend(RESEND_API_KEY);
     await resend.emails.send({
       from: 'noreply@auto.ellazoltan.com',
       to: 'info@ellazoltan.com',
@@ -93,10 +102,33 @@ const submitRsvp = async (req: RsvpSubmissionRequest, res: Response) => {
       html: emailBody,
     });
   } catch (error) {
-    /* empty */
+    console.error(error);
+    emailError = error as Error;
   }
 
-  return res.status(200).json({ message: 'Successfully saved RSVP form data' });
+  if (!saveError && !emailError) {
+    return res
+      .status(200)
+      .json({ message: 'Successfully saved & backup emailed RSVP form data' });
+  } else if (saveError && !emailError) {
+    return res.status(200).json({
+      message: 'Successfully backup emailed RSVP form data, but saving failed',
+      error: saveError.message,
+    });
+  } else if (emailError && !saveError) {
+    return res.status(200).json({
+      message: 'Successfully saved RSVP form data, but backup emailing failed',
+      error: emailError.message,
+    });
+  } else {
+    return res.status(500).json({
+      message: 'Both saving & backup emailing of RSVP form data failed',
+      errors: {
+        saveError: saveError!.message,
+        emailError: emailError!.message,
+      },
+    });
+  }
 };
 
 export default {
