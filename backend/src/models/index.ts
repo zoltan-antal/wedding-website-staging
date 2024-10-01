4e00ccbf094979ba8c2ee7c92af31e51b889ec27
zoltan-antal <zoltan_antal@outlook.com>
2024-10-01 23:37:39 +0100

    [backend] Add RSVP database schema

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,6 +1,7 @@
 import Household from './households';
 import Guest from './guests';
 import NameVariation from './nameVariations';
+import Rsvp from './rsvp';
 
 Guest.belongsTo(Household);
 Household.hasMany(Guest);
@@ -8,13 +9,18 @@ Household.hasMany(Guest);
 NameVariation.belongsTo(Guest);
 Guest.hasMany(NameVariation);
 
+Rsvp.belongsTo(Household);
+Rsvp.belongsTo(Guest);
+Household.hasMany(Rsvp);
+Guest.hasMany(Rsvp);
+
 Household.sync({ alter: true })
   .then(() => {
     console.log('Household table sync successful');
   })
   .catch((err) => {
     console.log('Household table sync failed:');
-    console.log(err);
+    console.error(err);
   });
 Guest.sync({ alter: true })
   .then(() => {
@@ -22,7 +28,7 @@ Guest.sync({ alter: true })
   })
   .catch((err) => {
     console.log('Guest table sync failed:');
-    console.log(err);
+    console.error(err);
   });
 NameVariation.sync({ alter: true })
   .then(() => {
@@ -30,7 +36,15 @@ NameVariation.sync({ alter: true })
   })
   .catch((err) => {
     console.log('NameVariation table sync failed:');
-    console.log(err);
+    console.error(err);
+  });
+Rsvp.sync({ alter: true })
+  .then(() => {
+    console.log('Rsvp table sync successful');
+  })
+  .catch((err) => {
+    console.log('Rsvp table sync failed:');
+    console.error(err);
   });
 
-export { Household, Guest, NameVariation };
+export { Household, Guest, NameVariation, Rsvp };
---
+++ backend/src/models/rsvp.ts
@@ -0,0 +1,39 @@
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  DataTypes,
+  CreationOptional,
+  ForeignKey,
+} from 'sequelize';
+import { sequelize } from '../utils/db';
+import Household from './households';
+import Guest from './guests';
+
+class Rsvp extends Model<InferAttributes<Rsvp>, InferCreationAttributes<Rsvp>> {
+  declare id: CreationOptional<number>;
+  declare guestId: ForeignKey<Guest['id']>;
+  declare householdId: ForeignKey<Household['id']>;
+}
+
+Rsvp.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    createdAt: true,
+    updatedAt: false,
+    modelName: 'rsvp',
+  }
+);
+
+export default Rsvp;
+
+export type RsvpAttributes = InferAttributes<Rsvp>;
+export type RsvpCreationAttributes = InferCreationAttributes<Rsvp>;
