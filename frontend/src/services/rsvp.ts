ac8cad7f3eb724e1419e0eea71f2a8de47eb3cf3
Zoltan Antal <zoltan_antal@outlook.com>
2024-10-02 15:38:14 +0100

    [frontend] Amend field name

--- frontend/src/services/rsvp.ts
+++ frontend/src/services/rsvp.ts
@@ -6,14 +6,14 @@ import { Language } from '../types/language';
 const baseUrl = `${config.BACKEND_URL}/rsvp`;
 
 async function submitRsvp(
-  rsvpData: RsvpFormData,
+  formData: RsvpFormData,
   emailCopy: boolean,
   language: Language
 ) {
   const requestConfig = { withCredentials: true };
   const response = await axios.post(
     `${baseUrl}`,
-    { rsvpData, emailCopy, language },
+    { formData, emailCopy, language },
     requestConfig
   );
   return response.data;
