f61234352a413dfb90ab4d21c3f8c95cec236907
zoltan-antal <zoltan_antal@outlook.com>
2024-10-07 20:50:24 +0100

    [frontend] Add content to and style contact page

--- frontend/src/components/Contact/index.tsx
+++ frontend/src/components/Contact/index.tsx
@@ -1,7 +1,66 @@
+import { useOutletContext, NavLink } from 'react-router-dom';
+import { Context } from '../../types/context';
+import './indec.css';
+
 const Contact = () => {
+  const { language, mainRef, navWidth } = useOutletContext<Context>();
+
   return (
-    <main>
-      <h1>Contact</h1>
+    <main ref={mainRef} style={{ width: `${navWidth}px` }} id="contact-page">
+      <h1>{{ English: 'Contact', Hungarian: 'Kapcsolat' }[language]}</h1>
+      <div>
+        <p>
+          {
+            {
+              English: 'Please RSVP by using the',
+              Hungarian: 'Kérjük, részvételi visszajelzésre használd a',
+            }[language]
+          }{' '}
+          <NavLink to={'/rsvp'}>
+            {
+              {
+                English: 'RSVP form',
+                Hungarian: 'visszajelzés űrlapot',
+              }[language]
+            }
+          </NavLink>
+          {'.'}
+        </p>
+        <p>
+          {
+            {
+              English:
+                'If you have any questions about the day, any arrangements or the lead-up and post-wedding period, check out the relevant pages and the',
+              Hungarian:
+                'Ha bármilyen kérdésed van a esküvővel, az előkészületekkel vagy az esküvő előtti és utáni időszakkal kapcsolatban, kérjük tekintsd át a releváns oldalakat és a',
+            }[language]
+          }{' '}
+          <NavLink to={'/faq'}>
+            {
+              {
+                English: 'FAQ page',
+                Hungarian: 'gyakran ismételt kérdések oldalt',
+              }[language]
+            }
+          </NavLink>
+          {'.'}
+        </p>
+        <p>
+          {' '}
+          {
+            {
+              English:
+                'If you still need to contact us, you can reach us via email at',
+              Hungarian:
+                'Ha ennek ellenére is szeretnéd felvenni velünk a kapcsolatot, kérjük, írj az alábbi e-mail címre:',
+            }[language]
+          }
+          <br />
+          <NavLink to={'mailto:info@ellazoltan.com'} id="email">
+            info@ellazoltan.com
+          </NavLink>
+        </p>
+      </div>
     </main>
   );
 };
