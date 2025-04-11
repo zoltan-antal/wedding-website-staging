43532f739b9ee87e19c271a8dd1c67ef5d2aeb51
Zoltan Antal <zoltan_antal@outlook.com>
2025-04-11 14:53:32 +0100

    [frontend] Add gift type

--- frontend/src/services/gift.ts
+++ frontend/src/services/gift.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import config from '../utils/config';
+import { Gift } from '../types/gift';
 
 const baseUrl = `${config.BACKEND_URL}/gifts`;
 
-async function fetchAllGifts() {
+async function fetchAllGifts(): Promise<Gift[]> {
   const requestConfig = { withCredentials: true };
   const response = await axios.get(`${baseUrl}/`, requestConfig);
-  return response.data;
+  return response.data as Gift[];
 }
 
 async function claimGift(giftId: number) {
