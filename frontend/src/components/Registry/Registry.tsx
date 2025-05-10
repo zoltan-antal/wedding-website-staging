522056d9d6d7454d324e3d140b6d22429626ac8e
zoltan-antal <zoltan_antal@outlook.com>
2025-05-10 15:31:30 +0100

    [frontend] Add text to and style Registry page

--- frontend/src/components/Registry/Registry.tsx
+++ frontend/src/components/Registry/Registry.tsx
@@ -6,7 +6,7 @@ import GiftList from './GiftList';
 import './Registry.css';
 
 const Registry = () => {
-  const { isInitialised, guest, household, language } =
+  const { isInitialised, guest, household, language, mainRef, navWidth } =
     useOutletContext<Context>();
   const [claiming, setClaiming] = useState(false);
 
@@ -23,7 +23,12 @@ const Registry = () => {
   }
 
   return (
-    <main id="registry-page" className={claiming ? 'claiming' : ''}>
+    <main
+      ref={mainRef}
+      style={{ width: `${navWidth}px` }}
+      id="registry-page"
+      className={claiming ? 'claiming' : ''}
+    >
       <h1>
         {
           {
@@ -32,6 +37,26 @@ const Registry = () => {
           }[language]
         }
       </h1>
+      <p>
+        {
+          {
+            English:
+              "As we've been living together for a long time, we already have most household items that people usually ask for as wedding gifts, so our gift list isn't very long. If you'd prefer — or if the item you wanted to gift has already been reserved by someone else — you're also welcome to gift us money, which we will put towards our honeymoon.",
+            Hungarian:
+              'Mivel már régóta együtt élünk, szinte már minden háztartási eszközünk megvan, amit általában ajándékba szoktak kérni, ezért az ajándéklistánk nem túl hosszú. Ha úgy preferálod, — vagy ha az általad kiszemelt ajándék már le van foglalva — pénzt is ajándékozhatsz: ezt a nászutunkra tervezzük költeni.',
+          }[language]
+        }
+      </p>
+      <p>
+        {
+          {
+            English:
+              'In the email sent about the gift registry, you can find the delivery address for the gifts and bank account details if you wish to transfer a monetary gift.',
+            Hungarian:
+              'Az ajándéklistáról szóló e-mailben megtalálod a szállítási címet az ajándékokhoz és a bankszámlaszámunkat (arra az esetre, ha átutalással szeretnél pénzbeli ajándékot adni).',
+          }[language]
+        }
+      </p>
       <GiftList claiming={claiming} setClaiming={setClaiming} />
     </main>
   );
